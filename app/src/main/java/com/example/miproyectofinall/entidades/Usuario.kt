f382a78b211654d5e1910f96b3dfcea8c255b447
Geraldine <geraldine.porras13474@est.uca.edu.ni>
2023-05-08 15:25:57 -0600

    Modificacion entidad

--- app/src/main/java/com/example/miproyectofinall/entidades/Usuario.kt
+++ app/src/main/java/com/example/miproyectofinall/entidades/Usuario.kt
@@ -8,17 +8,22 @@ import androidx.room.PrimaryKey
 data class Usuario(
     @PrimaryKey(autoGenerate = true)
     val id : Int,
+    @ColumnInfo(name = "nombreUsuario")
+    val nombreUsuario: String,
     @ColumnInfo(name = "nombreCompleto")
     val nombreCompleto: String,
     @ColumnInfo(name="userToken")
     val userToken: String,
-    @ColumnInfo(name = "nombreUsuario")
-    val nombreUsuario: String,
     @ColumnInfo(name="email")
     val email:String,
     @ColumnInfo(name = "urlPerfil")
     val urlPerfil:String,
     @ColumnInfo(name="numRepositorios")
-    val numRepositorios: Int
-
+    val numRepositorios: Int,
+    @ColumnInfo(name="numSeguidores")
+    val numSeguidores: Int,
+    @ColumnInfo(name="numSeguidos")
+    val numSeguidos: Int,
+    @ColumnInfo(name="location")
+    val location: String
 )
